67d7fbd74f2c2c6476ab9342769c11351cf3a34f
Uri <oriol253@gmail.com>
2022-12-03 15:57:06 +0100

    Tiles and items can hold multiple colliders

---
+++ Output/Assets/Maps/Mountain/ItemHitboxProvider.tsx
@@ -0,0 +1,112 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="ItemHitboxProvider" tilewidth="32" tileheight="32" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="../../Animations/Items/coin0_rotating000.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="2" name="5" x="4" y="12">
+    <polygon points="0,0 0,9 2,13 5,16 9,18 16,18 18,17 23,12 24,10 24,-1 23,-3 18,-8 16,-9 9,-9 5,-7 2,-4 1,-2"/>
+   </object>
+   <object id="3" name="4" x="9" y="13">
+    <polygon points="0,0 0,7 1,11 2,14 5,17 11,17 14,14 15,12 16,9 16,-2 15,-5 14,-7 11,-10 5,-10 2,-7 1,-4"/>
+   </object>
+   <object id="4" name="3" x="11" y="9">
+    <polygon points="0,0 0,15 1,17 3,20 4,21 6,21 7,20 8,18 9,17 10,15 10,0 9,-2 8,-3 7,-5 6,-6 4,-6 3,-5 2,-3 1,-2"/>
+   </object>
+   <object id="5" name="2" x="8" y="11">
+    <polygon points="0,0 0,11 1,14 2,16 5,19 11,19 14,16 15,13 16,9 16,2 15,-2 14,-5 11,-8 5,-8 2,-5 1,-3"/>
+   </object>
+   <object id="6" name="1" x="5" y="11">
+    <polygon points="0,0 0,11 1,13 6,18 8,19 15,19 17,18 19,17 20,16 22,14 23,12 24,10 24,1 23,-1 22,-3 21,-4 20,-5 19,-6 17,-7 15,-8 8,-8 6,-7 1,-2"/>
+   </object>
+   <object id="7" name="0" x="3" y="13">
+    <polygon points="0,0 1,-3 7,-9 10,-10 17,-10 20,-9 26,-3 27,0 27,7 26,10 20,16 17,17 10,17 7,16 1,10 0,7"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="../../Animations/Items/coin0_rotating001.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="1" x="5" y="11">
+    <polygon points="0,0 0,11 1,13 6,18 8,19 15,19 17,18 19,17 20,16 22,14 23,12 24,10 24,1 23,-1 22,-3 21,-4 20,-5 19,-6 17,-7 15,-8 8,-8 6,-7 1,-2"/>
+   </object>
+   <object id="2" name="0" x="4" y="14">
+    <polygon points="0,0 1,-3 7,-9 10,-10 17,-10 20,-9 26,-3 27,0 27,7 26,10 20,16 17,17 10,17 7,16 1,10 0,7"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="../../Animations/Items/coin0_rotating002.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="2" x="9" y="11">
+    <polygon points="0,0 0,11 1,14 2,16 5,19 11,19 14,16 15,13 16,9 16,2 15,-2 14,-5 11,-8 5,-8 2,-5 1,-3"/>
+   </object>
+   <object id="2" name="1" x="6" y="12">
+    <polygon points="0,0 0,11 1,13 6,18 8,19 15,19 17,18 19,17 20,16 22,14 23,12 24,10 24,1 23,-1 22,-3 21,-4 20,-5 19,-6 17,-7 15,-8 8,-8 6,-7 1,-2"/>
+   </object>
+   <object id="3" name="0" x="5" y="15">
+    <polygon points="0,0 1,-3 7,-9 10,-10 17,-10 20,-9 26,-3 27,0 27,7 26,10 20,16 17,17 10,17 7,16 1,10 0,7"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="../../Animations/Items/coin0_rotating003.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="3" x="12" y="9">
+    <polygon points="0,0 0,15 1,17 3,20 4,21 6,21 7,20 8,18 9,17 10,15 10,0 9,-2 8,-3 7,-5 6,-6 4,-6 3,-5 2,-3 1,-2"/>
+   </object>
+   <object id="2" name="2" x="9" y="11">
+    <polygon points="0,0 0,11 1,14 2,16 5,19 11,19 14,16 15,13 16,9 16,2 15,-2 14,-5 11,-8 5,-8 2,-5 1,-3"/>
+   </object>
+   <object id="3" name="1" x="6" y="12">
+    <polygon points="0,0 0,11 1,13 6,18 8,19 15,19 17,18 19,17 20,16 22,14 23,12 24,10 24,1 23,-1 22,-3 21,-4 20,-5 19,-6 17,-7 15,-8 8,-8 6,-7 1,-2"/>
+   </object>
+   <object id="4" name="0" x="5" y="15">
+    <polygon points="0,0 1,-3 7,-9 10,-10 17,-10 20,-9 26,-3 27,0 27,7 26,10 20,16 17,17 10,17 7,16 1,10 0,7"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="../../Animations/Items/coin0_rotating004.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="2" name="4" x="9" y="13">
+    <polygon points="0,0 0,7 1,11 2,14 5,17 11,17 14,14 15,12 16,9 16,-2 15,-5 14,-7 11,-10 5,-10 2,-7 1,-4"/>
+   </object>
+   <object id="3" name="3" x="12" y="9">
+    <polygon points="0,0 0,15 1,17 3,20 4,21 6,21 7,20 8,18 9,17 10,15 10,0 9,-2 8,-3 7,-5 6,-6 4,-6 3,-5 2,-3 1,-2"/>
+   </object>
+   <object id="4" name="2" x="9" y="11">
+    <polygon points="0,0 0,11 1,14 2,16 5,19 11,19 14,16 15,13 16,9 16,2 15,-2 14,-5 11,-8 5,-8 2,-5 1,-3"/>
+   </object>
+   <object id="5" name="1" x="6" y="12">
+    <polygon points="0,0 0,11 1,13 6,18 8,19 15,19 17,18 19,17 20,16 22,14 23,12 24,10 24,1 23,-1 22,-3 21,-4 20,-5 19,-6 17,-7 15,-8 8,-8 6,-7 1,-2"/>
+   </object>
+   <object id="6" name="0" x="5" y="15">
+    <polygon points="0,0 1,-3 7,-9 10,-10 17,-10 20,-9 26,-3 27,0 27,7 26,10 20,16 17,17 10,17 7,16 1,10 0,7"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="../../Animations/Items/coin0_rotating005.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="5" x="5" y="12">
+    <polygon points="0,0 0,9 2,13 5,16 9,18 16,18 18,17 23,12 24,10 24,-1 23,-3 18,-8 16,-9 9,-9 5,-7 2,-4 1,-2"/>
+   </object>
+   <object id="2" name="4" x="10" y="14">
+    <polygon points="0,0 0,7 1,11 2,14 5,17 11,17 14,14 15,12 16,9 16,-2 15,-5 14,-7 11,-10 5,-10 2,-7 1,-4"/>
+   </object>
+   <object id="3" name="3" x="13" y="10">
+    <polygon points="0,0 0,15 1,17 3,20 4,21 6,21 7,20 8,18 9,17 10,15 10,0 9,-2 8,-3 7,-5 6,-6 4,-6 3,-5 2,-3 1,-2"/>
+   </object>
+   <object id="4" name="2" x="10" y="12">
+    <polygon points="0,0 0,11 1,14 2,16 5,19 11,19 14,16 15,13 16,9 16,2 15,-2 14,-5 11,-8 5,-8 2,-5 1,-3"/>
+   </object>
+   <object id="5" name="1" x="7" y="13">
+    <polygon points="0,0 0,11 1,13 6,18 8,19 15,19 17,18 19,17 20,16 22,14 23,12 24,10 24,1 23,-1 22,-3 21,-4 20,-5 19,-6 17,-7 15,-8 8,-8 6,-7 1,-2"/>
+   </object>
+   <object id="6" name="0" x="6" y="16">
+    <polygon points="0,0 1,-3 7,-9 10,-10 17,-10 20,-9 26,-3 27,0 27,7 26,10 20,16 17,17 10,17 7,16 1,10 0,7"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
