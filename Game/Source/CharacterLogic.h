3a9ac93ad0a24483a42a5d1b0ef0f256de6ba93d
Uri <oriol253@gmail.com>
2022-12-10 15:49:00 +0100

    Templated Adjacency List

--- Game/Source/CharacterLogic.h
+++ Game/Source/CharacterLogic.h
@@ -8,9 +8,7 @@
 #include <algorithm>
 #include <any>
 
-class CharacterLogic;
-
-
+/*
 template<class UnaryPred>
 std::any Find2(std::string_view action, UnaryPred predicate)
 {
@@ -19,65 +17,92 @@ std::any Find2(std::string_view action, UnaryPred predicate)
 		return StrEquals(v->action, action);
 	}
 }
+*/
 
+template <typename T>
 struct Vertex
 {
 public:
-	explicit Vertex(std::string_view name) : action(name) {};
+	explicit Vertex(T name) : action(name) {};
 
-	uint AddEdge(std::string_view destination)
+	int AddEdge(int destination)
 	{
-		edges.push_back(1);
-		return static_cast<uint>(edges.size()) - 1;
+		edges.push_back(destination);
+		return static_cast<int>(edges.size()) - 1;
 	}
 
-	uint ActionToIndex;
-
-	std::string action = "";
-	std::vector<uint> edges;
-	friend class AdjacencyList;
+	T action;
+	std::vector<int> edges;
 };
 
+template <typename T>
+struct graphvector : std::vector<std::unique_ptr<Vertex<T>>>
+{
+	std::vector<std::unique_ptr<Vertex<T>>> &operator[](std::size_t i)
+	{
+
+	}
+};
+
+template <typename T>
 class AdjacencyList
 {
 public:
-	using graph2d = std::vector<std::unique_ptr<Vertex>>;
+	using graph2d = std::vector<std::unique_ptr<Vertex<T>>>;
 
 	AdjacencyList() = default;
 	~AdjacencyList() = default;
 
-	int AddVertex(std::string_view action)
+	int AddVertex(T action)
 	{
 		if(auto i = GetIndex(action); i >= 0)
 			return i;
 
-		graph.push_back(std::make_unique<Vertex>(action));
-		return graph.size() - 1;
+		graph.push_back(std::make_unique<Vertex<T>>(action));
+		return static_cast<int>(graph.size()) - 1;
 	}
 
-	int AddEdge(std::string_view origin, std::string_view destination)
+	int AddEdge(T origin, T destination)
 	{
-		if(auto v = At(origin); v)
-			return v->AddEdge(destination);
+		Vertex<T> *v = At(origin);
+		if(!v)
+		{
+			AddVertex(origin);
+			v = At(origin);
+		}
+
+		// If destination exists
+		if(auto i = GetIndex(destination); i != -1)
+		{
+			return v->AddEdge(i);
+		}
+		// If destination doesn't exist we add it.
+		return v->AddEdge(AddVertex(destination));
+
+	}
+
+	int AddVertexAndEdge(T origin, T destination, int first, int second)
+	{
+
 	}
 
 	// Returns index if action is on the array, -1 if no match
-	int GetIndex(std::string_view action) const
+	int GetIndex(T action) const
 	{
 		for(int i = 0; auto const &elem : graph)
 		{
-			if(StrEquals(elem.get()->action, action))
+			if(elem.get()->action == action)
 				return i;
 		}
 		return -1;
 	}
 
 	// Returns iterator to element, iterator == graph.last() if no match
-	graph2d::iterator Find(std::string_view action)
+	graph2d::iterator Find(T action)
 	{
 		for(auto it = graph.begin(); it != graph.end(); ++it)
 		{
-			if(StrEquals((*it)->action, action))
+			if((*it)->action == action)
 			{
 				return it;
 			}
@@ -86,12 +111,12 @@ public:
 	}
 
 	// Returns vertex of action, nullptr if no match
-	Vertex *At(std::string_view action)
+	Vertex<T> *At(T action)
 	{
 		if(auto it = std::ranges::find_if(graph.begin(), graph.end(),
-										  [action](std::unique_ptr<Vertex> const &v)
+										  [action](std::unique_ptr<Vertex<T>> const &v)
 										  {
-											  return StrEquals(v->action, action);
+											  return v->action == action;
 										  }
 		); it != graph.end())
 			return it->get();
@@ -100,7 +125,7 @@ public:
 	}
 
 private:
-	uint currentAction = 0;
-	graph2d graph;
+	int currentAction = 0;
+	std::vector<std::unique_ptr<Vertex<T>>> graph;
 };
 #endif	// __CHARACTERLOGIC_H__
